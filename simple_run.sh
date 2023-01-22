6d9cf4452b0374adf0a953bbb6f4eafd019dd0b9
Jakub <254488@student.pwr.edu.pl>
2023-01-22 15:26:29 +0100

    add safe check and doc comm

--- simple_run.sh
+++ simple_run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-# script to
+# script to run application with pure python, without docker
 
-path=$(realpath $1)
-
-if test -f "$path"; then
-    python main.py $path
+if [ "$#" -lt 1 ]; then
+    echo "Please provide path to a file with map, e.g. '/../islands.txt'"
+elif [ "$#" -ne 1 ]; then
+    echo "Please provide only one parameter - path to a file with map, e.g. '/../islands.txt'"
 else
-    echo "Provided path to file does not exists or points to a directory!"
+    path=$(realpath $1) &> /dev/null
+
+    if test -f "$path"; then
+        python main.py $path
+    else
+        echo "Provided path to file does not exists or points to a directory!"
+    fi
 fi
