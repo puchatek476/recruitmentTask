4c915e834e244ff8b21360b73af9cbb5beaa56b9
Jakub <254488@student.pwr.edu.pl>
2023-01-22 15:29:11 +0100

    add help comm

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # script which creates islands-counter-app container and runs it with provided islands grid file as first param
+# containers shuts down and removes itselfs automatically
 
 if [ "$#" -lt 1 ]; then
     echo "Please provide path to a file with map, e.g. '/../islands.txt'"
@@ -8,6 +9,7 @@ elif [ "$#" -ne 1 ]; then
 else
     path=$(realpath $1)
     if test -f "$path"; then
+        # delete MSYS_NO_PATHCONV=1 part if met some issues
         MSYS_NO_PATHCONV=1 docker run --rm --name islands-counter-app -v $path:/application/islands.txt islands-counter islands.txt
     else
         echo "Provided path to file does not exists or points to a directory!"
